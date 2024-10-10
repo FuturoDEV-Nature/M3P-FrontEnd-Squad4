13026a9683122b6d1e11730695d5e10ecb79f8f2
concs-niemeyer <conrado.raizen@gmail.com>
2024-10-10 20:46:08 -0300

    token login integrado - deploy

--- src/context/authContext.jsx
+++ src/context/authContext.jsx
@@ -14,26 +14,43 @@ export function AuthProvider({ children }) {
   );
   const [erroLogin, setErroLogin] = useState(null);
 
-  async function Login({ email, senha, token }) {
-    try {
-      const response = await axios.post(
-        "https://m3p-backend-squad4-dhih.onrender.com/login",
-        data
-      );
-	  const token = req.Authorization
-	  console.log(":::TOKEN:::",token)
-      const user = response.data.find(
-        (user) => user.email === email && user.senha === senha
-      );
+  async function Login({ email, senha}) {
+	const {register, handleSubmit, formState: {errors}, reset} = useForm()
 
-      if (user) {
-        setUsuario(user);
-        setErroLogin(false);
-        localStorage.setItem("usuario", JSON.stringify(user));
-      } else {
-        setErroLogin(true);
-      }
-    } catch (error) {
+    async function validarLogin(dados){
+        try {
+            const response = await axios.post("https://api-nature-trip-revisao.onrender.com/login", {
+                email: dados.email,
+                senha: dados.senha
+            })
+
+            if(response.status == 200){
+                localStorage.setItem('token', response.data.token)
+                localStorage.setItem('user', JSON.stringify(response.data.user))
+
+                // redirect para o dashboard
+                window.location.href = "/"
+            }
+        }
+    // try {
+    //   const response = await axios.post(
+    //     "https://m3p-backend-squad4-dhih.onrender.com/login",
+    //     data
+    //   );
+	//   const token = req.Authorization
+	//   console.log(":::TOKEN:::",token)
+    //   const user = response.data.find(
+    //     (user) => user.email === email && user.senha === senha
+    //   );
+
+    //   if (user) {
+    //     setUsuario(user);
+    //     setErroLogin(false);
+    //     localStorage.setItem("usuario", JSON.stringify(user));
+    //   } else {
+    //     setErroLogin(true);
+    //   }
+    catch (error) {
       console.error("Erro na autenticação", error);
       setErroLogin(true);
     }
@@ -50,7 +67,7 @@ export function AuthProvider({ children }) {
     </AuthContext.Provider>
   );
 }
-
+}
 export function useAuth() {
   const contexto = useContext(AuthContext);
   return contexto;
