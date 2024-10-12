1ec7e6e738a1c643cf9477c422c92a6f9d24594a
Geeobarros <ge.barros96@gmail.com>
2024-10-12 13:49:17 -0300

    deixando link do back end para testes

--- src/api/endpoints.js
+++ src/api/endpoints.js
@@ -1,91 +1,86 @@
 import axios from "axios";
 
-<<<<<<< Updated upstream
 const urlPrefix = "http://localhost:3000";
-=======
-const urlPrefix = "https://localhost:3000";
->>>>>>> Stashed changes
+// const urlPrefix = "https://m3p-backend-squad4-t6lg.onrender.com";
 
-export const getUsers = async () => await axios.get(`${urlPrefix}/usuarios`)
+export const getUsers = async () => await axios.get(`${urlPrefix}/user`)
     .then(function (response) {
-       return response.data
-    
-    
+       return response.data   
+
+       
     })
     .catch(function (error) {
-   
-    console.error(error);
+        console.error(error);
     })
 
-export const getUser = async (id) => await axios.get(`${urlPrefix}/usuarios/${id}`)
-    .then(function (response) {
-    
-    console.log(response);
-    })
-    .catch(function (error) {
-    
-    console.error(error);
-    })
+export const getUser = (id) => new Promise((resolve, reject) => {
+    axios.get(`${urlPrefix}/user/${id}`)
+        .then(function (response) {
+            resolve(response);
+        })
+        .catch(function (error) {
+            console.error(error);
+            reject(error);
+        })
 
-export const addUser = async (values) => await axios.post(`${urlPrefix}/usuarios/`, values)
+}) 
+
+export const addUser = async (values) => await axios.post(`${urlPrefix}/user/`, values)
     .then(function (response) {
         return response.data
-    
     })
     .catch(function (error) {
-    
-    console.error(error);
+        console.error(error);
     })
 
-export const editUser = async (id) => await axios.put(`${urlPrefix}/usuarios/${id}`)
+export const editUser = async (id) => await axios.put(`${urlPrefix}/user/${id}`)
     .then(function (response) {
-    
-    console.log(response);
+        console.log(response);
     })
     .catch(function (error) {
-    
-    console.error(error);
+        console.error(error);
     })
 
-export const deleteUser = async (id) => await axios.delete(`${urlPrefix}/usuarios/${id}`)
+export const deleteUser = async (id) => await axios.delete(`${urlPrefix}/user/${id}`)
     .then(function (response) {
-    
-    console.log(response);
+        console.log(response);
     })
     .catch(function (error) {
-    
-    console.error(error);
+        console.error(error);
     })
 
-export const getLocais = async () => await axios.get(`${urlPrefix}/locais`)
+export const getLocais = () => new Promise((resolve, reject) => {
+	axios.get(`${urlPrefix}/local`)
     .then(function (response) {
-        return response.data;
-    })
+        resolve(response.data)
+	}) 
     .catch(function (error) {
-    
-    console.error(error);
+       console.error(error);
+       reject(error)
     })
+})
 
-export const addLoccais = async (values) => await axios.post(`${urlPrefix}/locais`, values)
+export const addLoccais = (values) => new Promise((resolve, reject) => {
+    axios.post(`${urlPrefix}/local`, values)
     .then(function (response) {
-        return response.data;
+        resolve(response.data);
     })
     .catch(function (error) {    
-    console.error(error);
+        console.error(error);
+        reject(error)
     })
+}) 
 
-export const deleteLocal = async (id) => await axios.delete(`${urlPrefix}/locais/${id}`)
+export const deleteLocal = async (id) => await axios.delete(`${urlPrefix}/local/${id}`)
     .then(function (response) {
         return response.data;
-    
     })
     .catch(function (error) {
-    
-    console.error(error.message);
+        console.error(error.message);
     })
     
 export const atualizarLocal = async (id, data) =>  await axios
-    .put(`${urlPrefix}/locais/${id}`, data)
+    .put(`${urlPrefix}/locals/${id}`, data)
     .then(function (response) {
     return response.data;
 })
