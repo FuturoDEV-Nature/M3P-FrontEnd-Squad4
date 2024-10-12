2ef23a0542390fd1ee3269a064548cc48f7dd2ba
Geeobarros <ge.barros96@gmail.com>
2024-10-12 20:01:41 -0300

    fix details

--- src/context/authContext.jsx
+++ src/context/authContext.jsx
@@ -15,7 +15,7 @@ export function AuthProvider({ children }) {
 
   async function Login({ email, senha }) {
     try {
-      const response = await axios.post(`${urlPrefix}/login`, { email, senha });
+      const response = await axios.post(`https://m3p-backend-squad4-t6lg.onrender.com/login`, { email, senha });
       console.log(response)
       
       const token = response.data.token;
